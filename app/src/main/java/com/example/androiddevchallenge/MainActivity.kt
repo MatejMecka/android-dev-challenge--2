ee612d859c2658a2c59e238d4b665ac9f6504876
Matej Plavevski <matej.plavevski@gmail.com>
2021-03-04 00:15:09 +0100

    nesh

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -1,4 +1,4 @@
-/*
+ /*
  * Copyright 2021 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,12 +18,24 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,9 +51,41 @@ class MainActivity : AppCompatActivity() {
 // Start building your app here!
 @Composable
 fun MyApp() {
+    //var
+    var counter by remember { mutableStateOf(60) }
+    var started by remember { mutableStateOf(false) }
+
+    val progress by animateFloatAsState(
+        targetValue = 1f,
+        animationSpec = tween(durationMillis = 1000, easing = LinearEasing)
+    )
+
     Surface(color = MaterialTheme.colors.background) {
+        Column(){
         Text(text = "Ready... Set... GO!")
+            Box(Modifier.fillMaxSize()) {
+                CircularProgressIndicator(
+                    progress = counter,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(32.dp)
+                )
+                Text(text = "${counter}")
+            }
+        }
     }
+
+    if(started == false){
+        LaunchedEffect("tick") {
+            for(i in counter downTo 0){
+                delay(1000)
+                counter = i
+                println(counter)
+            }
+            //started = false
+        }
+    }
+
 }
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
